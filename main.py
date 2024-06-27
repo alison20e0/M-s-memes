63d981a18d965d0cc6573b473be4bc86dbe224cc
alison20e0 <alison.estrella2006@gmail.com>
2024-06-27 02:12:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+import os
+from discord.ext import commands
+import random
+
+description = "Este es un programa donde vinculamos a Discord con VS Code para lanzar imagenes"
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="$", description=description, intents=intents)
+img_mem = os.listdir("images")
+img_ani = os.listdir("animales")
+
+@bot.event
+async def on_ready():
+    print(f"Logueado como {bot.user} (ID: {bot.user.id})")
+
+@bot.command()
+async def mem(ctx):
+    if ctx.message.author == bot.user:
+        return
+    
+    img_to_send = random.choice(img_mem)
+    with open(f'images/{img_to_send}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+@bot.command()
+async def animal(ctx):
+    if ctx.message.author == bot.user:
+        return
+    
+    img_to_send_ani = random.choice(img_ani)
+    with open(f'animales/{img_to_send_ani}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+bot.run("Token")
